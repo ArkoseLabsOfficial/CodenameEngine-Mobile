c230a7dc154176c38f2e96150b3de8a679d78f0e
MaysLastPlay <109207233+MaysLastPlay@users.noreply.github.com>
2026-04-10 21:52:37 +0300

    clearly smth there

--- source/funkin/backend/assets/ModsFolder.hx
+++ source/funkin/backend/assets/ModsFolder.hx
@@ -27,11 +27,11 @@ class ModsFolder {
 	/**
 	 * Path to the `mods` folder.
 	 */
-	public static var modsPath:String = #if mobile MobileUtil.getDirectory() + "mods/" #else "./mods/" #end;
+	public static var modsPath:String = #if mobile MobileUtil.getDirectory() + #end "./mods/";
 	/**
 	 * Path to the `addons` folder.
 	 */
-	public static var addonsPath:String = #if mobile MobileUtil.getDirectory() + "addons/" #else "./addons/" #end;
+	public static var addonsPath:String = #if mobile MobileUtil.getDirectory() + #end "./addons/";
 
 	/**
 	 * If accessing a file as assets/data/global/LIB_mymod.hx should redirect to mymod:assets/data/global.hx
